581aab7ffe1afd6060b0d9f5b27bc5f4210deb91
SOUMAAA <106883759+Yanouui@users.noreply.github.com>
2022-06-14 23:00:35 +0200

    Add files via upload

---
+++ Enfant.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>espace enfant</title>
+    <link rel="stylesheet" href="Enfant.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+</head>
+
+<body>
+
+
+    <div class="wrapper">
+
+        <div class="multi_color_border"></div>
+        <div class="top_nav">
+            <div class="left">
+
+
+            </div>
+            <div class="right">
+                <ul>
+                    
+                    <li><a href="Home1.php">Nous Contacter</a></li>
+                    <li><a href="Home1.php">A propos de nous</a></li>
+                    <li><a href="signup.php">inscription</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <header>
+        <div class="nav container">
+            <a href="Home1.php" class="logo"><i class='bx bxs-home-alt-2'></i>GlutenFree</a>
+            <input type="checkbox" name="" id="menu">
+            <label for="menu" <i class='bx bx-menu' id="menu-icon"></i> </label>
+            <ul class="navbar">
+                <li>
+                    <a href="">Les Maladies</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="cœliaque.php">Maladie coeliaque</a>
+                            </li>
+                            <li> <a href="sensibilite.php">Sensibilité AU Gluten</a></li>
+                            <li> <a href="herpétiforme.php">Dermatite herpétiforme</a></li>
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="">nos espace</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="Enfant.php">espace enfant</a>
+                            </li>
+                            <li> <a href="point.php">Point de vente </a></li>
+                            <li> <a href="produitt.php">les Produit </a></li>
+
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="#home">Le gluten </a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="gulten.php">c'est quoi le gluten </a>
+                            </li>
+                            <li> <a href="source.php"> source du gluten</a></li>
+                           
+                            
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#home">Regime</a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="recette.php">Recette </a>
+                            </li>
+                            <li> <a href="Erreur.php"> Au quotidien</a></li>
+
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+
+
+
+        </div>
+
+    </header>
+<section>
+    <span class="image1">
+        <img src="pexels-lukas-296301 (1).jpg" width="100%" height="400" alt="">
+    </span>
+<h1>coliaque chez les enfant</h1>
+
+<p class="p3">
+    La maladie cœliaque peut se développer à tout moment après l'introduction de blé ou d'autres aliments contenant du
+    gluten dans l'alimentation, généralement après l'âge de 6 à 9 mois. On ne sait pas pourquoi certains enfants tombent
+    malades tôt dans la vie et d'autres ne tombent malades qu'après des années d'exposition. Il est très important de tester
+    votre enfant dès les premiers signes ou si la maladie coeliaque sévit dans votre famille. Les parents au premier degré
+    (parent, frère ou sœur, enfant) ont 1 chance sur 10 de développer eux-mêmes la maladie coeliaque.
+</p>
+
+<div class="container">
+    <div class="photo1">
+        <img src="sick.jpg" alt="" />
+    </div>
+    <br>
+    <p>
+        Il existe une grande variation dans la gravité des symptômes - de nombreux enfants ressentiront des symptômes quelques
+        minutes à quelques heures après avoir consommé du gluten, ce qui peut ne durer que quelques heures.
+        
+        Dans d'autres, les symptômes peuvent durer plusieurs jours ou jusqu'à deux semaines. De nombreux enfants présentent des
+        symptômes bénins qui passent facilement inaperçus, comme des gaz excessifs, des douleurs abdominales ou de la
+        constipation. D'autres enfants présentent des symptômes plus graves pouvant entraîner un diagnostic plus précoce,
+        notamment un retard de croissance, une perte de poids et des vomissements.
+        
+        Heureusement, les enfants et les adolescents répondent généralement bien au traitement par le régime sans gluten. La
+        plupart des enfants se sentent nettement mieux après deux semaines de régime et atteignent une taille, un poids et une
+        santé osseuse normaux.
+    </p>
+    <br>
+    <p>
+        Heureusement, les enfants et les adolescents répondent généralement bien au traitement par le régime sans gluten. La
+        plupart des enfants se sentent nettement mieux après deux semaines de régime et atteignent une taille, un poids et une
+        santé osseuse normaux.
+    </p> <br>
+    <span class="photo2">
+        <img src="schoolage.jpg" ></span>
+    
+    <h4>Enfants d'âge scolaire </h4> <br>
+    <p>
+    Les vomissements sont moins fréquents chez les enfants d'âge scolaire que chez les nourrissons et les tout-petits. Les
+    symptômes incluent, mais ne sont pas limités à :
+    <ul>
+        <li>
+       - Maux de ventre ou douleurs abdominales </li>
+        <li> - Distension abdominale</li>
+        <li>- La diarrhée</li>
+        <li>- Constipation</li>
+        <li>- Difficulté à prendre du poids ou à perdre du poids</li>
+        
+    </ul>
+    </p><br><br><br><br><br><br><br><br>
+<div class="photo1">
+    <img src="old.jpg" alt="" />
+</div>
+
+<h4>Enfants plus âgés et adolescents</h4> <br>
+<p>
+Les enfants plus âgés et les adolescents peuvent présenter des symptômes ou des signes qui ne sont pas manifestement
+liés au tractus intestinal, appelés symptômes « extra-intestinaux » ou « atypiques ».
+
+Ces symptômes sont ce qui peut convaincre un médecin de tester la maladie coeliaque. Certaines de ces manifestations
+comprennent :
+
+
+    <li> - Croissance ralentie</li>
+  <li>  - Perte de poids</li>
+ <li> - Puberté retardée</li>
+ <li> - Douleur douloureuse dans les os ou les articulations</li>
+ <li> - Fatigue chronique</li>
+ <li> - Maux de tête ou migraines récurrents</li>
+ <li> - Éruption cutanée avec démangeaisons (dermatite herpétiforme)</li>
+ <li> - Plaies buccales récurrentes, appelées ulcères aphteux, qui ressemblent à des aphtes</li>
+</p>
+<p>
+    Les adolescents atteints de la maladie coeliaque peuvent également avoir des troubles de l'humeur, notamment de
+    l'anxiété et de la dépression, ainsi que des crises de panique.
+
+</p> <br>
+
+</div>
+
+
+
+</section>
+<br><br>
+
+    
+<!--footerrrrrrr-->
+<section class="footer">
+    <br><br> <br>
+    <div class="footer-container container">
+        <h2>GlutenFree</h2>
+        <div class="footer-box">
+            <h3>Lien</h3>
+
+            <a href="cœliaque.php">Maladie coeliaque </a>
+            <a href="Galette.php">c'est quoi le gluten </a>
+            <a href="recette.php"> Recette</a>
+
+        </div>
+
+        <div class="footer-box">
+            <h3>location</h3>
+            <a href="#"> ALger </a>
+            <a href="#">Annaba</a>
+            <a href="#">Oran</a>
+
+
+        </div>
+
+
+        <div class="footer-box">
+            <h3>Contact</h3>
+            <a href="#">+213 (0)987 665 6543</a>
+            <a href="#">glutenfree@gmail.com</a>
+            <div class="social">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+</section>
+</body>
+</html>
---
+++ cœliaque.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>maladie Coeliaque</title>
+    <link rel="stylesheet" href="coeliaque.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <style>
+
+.IMAGE{
+ margin-top: 0px;
+  background: url(rrrrr.jpg);
+  background-repeat: no-repeat;
+  background-size:cover;
+ background-position: center;
+  
+  height: 440px;
+  
+  display: flex;
+  align-items: center;
+
+}
+
+    </style>
+</head>
+
+<body>
+
+
+    <div class="wrapper">
+
+        <div class="multi_color_border"></div>
+        <div class="top_nav">
+            <div class="left">
+
+
+            </div>
+            <div class="right">
+                <ul>
+                  
+                    <li><a href="Home1.php">Nous Contacter</a></li>
+                    <li><a href="Home1.php">A propos de nous</a></li>
+                    <li><a href="signup.php">inscription</a></li>
+                  
+                </ul>
+            </div>
+        </div>
+    </div>
+    <header>
+        <div class="nav container">
+            <a href="Home1.php" class="logo"><i class='bx bxs-home-alt-2'></i>GlutenFree</a>
+            <input type="checkbox" name="" id="menu">
+            <label for="menu" <i class='bx bx-menu' id="menu-icon"></i> </label>
+            <ul class="navbar">
+                <li>
+                    <a href="">Les Maladies</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="coeliaque.html">Maladie coeliaque</a>
+                            </li>
+                            <li> <a href="sensibilite.php">Sensibilité AU Gluten</a></li>
+                            <li> <a href="herpétiforme.php">Dermatite herpétiforme</a></li>
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="">nos espace</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="Enfant.php">espace enfant</a>
+                            </li>
+                            <li> <a href="point.php">Point de vente </a></li>
+                            <li> <a href="produitt.php">les Produit </a></li>
+
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="#home">Le gluten </a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="gulten.php">c'est quoi le gluten </a>
+                            </li>
+                            <li> <a href="source.php"> source du gluten</a></li>
+                           
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#home">Regime</a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="recette.php">Recette </a>
+                            </li>
+                            <li> <a href="Erreur.php"> Au quotidien</a></li>
+
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+
+
+
+        </div>
+
+
+
+
+
+
+    </header>
+    <section class="section">
+                   <span class="IMAGE">
+                       
+                   </span>
+                   <div class="TITLE">
+                       <h1>
+                
+                        La maladie cœliaque
+                       </h1> <br>
+                   </div>
+                  <br>
+                   <div class="soustitre">
+                 <h4>
+                    Définition 
+                 </h4>  <br>  
+                   </div>  <div class="parag">
+                  &nbsp;  La maladie cœliaque (MC) est un problème médical commun et touchant, selon les estimations, un pour cent de la population. Il s’agit d’un problème médical au cours duquel la surface d’absorption de l’intestin grêle est endommagée par une substance appelée gluten. Le gluten représente un groupe de protéines présentes dans le blé, le seigle et l’orge, ainsi que dans leurs céréales croisées. Les dommages infligés à l’intestin peuvent mener à divers symptômes et entraîner l’incapacité de l’organisme à absorber les nutriments comme les protéines, le gras, les glucides, les vitamines et les minéraux, essentiels à une bonne santé.
+                   </div> <br>
+                   <div class="soustitre1">
+                  <H4> 
+                    Comment se présente la maIadie coeliaque ?
+                   </H4>
+
+
+                   </div>  <br>
+                   <div class="parag1">
+                La forme du nourrisson et celle du jeune enfant est la plus classique. L’enfant présente souvent une diarrhée chronique
+                ; il est fatigué, apathique et triste. Son abdomen est ballonné et ses membres fins. On note le plus souvent un
+                ralentissement de la croissance en poids et en taille.
+                Chez l’enfant plus âgé, la maladie peut être peu symptomatique, limitée à une petite taille, un retard d’apparition de
+                la puberté, une anémie chronique, des anomalies de l’émail dentaire, des douleurs articulaires…
+                Chez l’adulte, les signes de la maladie peuvent être des ballonements, des épisodes diarrhée - constipation et un
+                amaigrissement inquiétant. La maladie peut être mono-symptomatique (anémie, ostéoporose…) ou atypique en se manifestant
+                par des crampes musculaires, une stomatite aphteuse, des irrégularités menstruelles, des fausses couches à répétition.
+                Le nombre et l’intensité des symptômes varient d’une personne à une autre.
+
+                   </div>  <br>
+                   <div class="soustitre2">
+                       <H4>
+                        Diagnostic
+                       </H4> </div> <br>
+                       <div class="parag2">
+                        (1). Dépistage
+                        Par le passé, les médecins devaient s’appuyer sur les symptômes et sur les signes cliniques pour suspecter un diagnostic de la MC; puis choisir quels patients devraient passer d’autres tests pour obtenir une confirmation. Maintenant, des tests sanguins très efficaces sont disponibles pour dépister la MC. L’anticorps IgA du tissu transglutaminase est le test de dépistage actuellement recommandé. Les patients doivent suivre un régime alimentaire normal (contenant du gluten) au moment du dépistage afin que le résultat soit valide.
+                        <br>
+                     (2). Biopsie
+                        Le diagnostic formel de la MC est établi grâce à une biopsie duodénale. La biopsie est effectuée par endoscopie par un gastroentérologue. Nous vous rappelons qu’il est primordial de continuer à consommer du gluten avant la biopsie, puisque l’arrêt de la consommation pourrait compromettre le diagnostic.
+                       </div> 
+                    <table class="content-table">
+                    
+                        <thead>
+                    
+                            <tr>
+                                <th> &nbsp;&nbsp;&nbsp; ÂGE DU DIAGNOSTIC &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                                <th> RISQUE DE DÉVELOPPER UNE AUTRE MALADIE AUTO-IMMUNE </th>
+                    
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="active-row">
+                                <td>2-4</td>
+                                <td>10.5%</td>
+                    
+                    
+                            </tr>
+                            <tr class="active-row">
+                                <td> 4-12</td>
+                                <td> 16.7% </td>
+                    
+                    
+                            </tr>
+                            <tr class="active-row">
+                                <td>12-20</td>
+                                <td>27%</td>
+                    
+                            </tr>
+                            <tr class="active-row">
+                                <td> Over 20</td>
+                                <td>34% </td>
+                    
+                            </tr>
+                        </tbody>
+                    </table>
+                       
+                       <br>
+                       <div class="soustitre3"> 
+                        
+                       <H4>
+                        Traitement
+                       </H4> <br>
+
+                       </div> 
+                       <div class="parag3">
+                        Il n’existe présentement aucun traitement curatif pour la MC; toutefois, elle peut être bien traitée à l’aide d’un régime alimentaire sans gluten. Le respect du régime alimentaire sans gluten doit être STRICT et À VIE. Un régime alimentaire sans gluten peut être exigeant et lourd; ainsi, il est primordial de consulter un diététiste professionnel ayant une expertise dans le régime alimentaire sans gluten.<BR>
+
+                        Les individus atteints de la MC doivent être vigilants face aux sources cachées de gluten, ainsi qu’à la contamination croisée des produits alimentaires. Il est essentiel de procéder à la lecture minutieuse des étiquettes à chaque achat de produit.
+                        
+                        Puisque la maladie cœliaque est un problème médical chronique, un suivi régulier à long terme par un médecin et un diététiste est recommandé.
+                        
+                        Développé par les conseillers consultatifs professionnels de l’ACMC
+                        juin 2016
+                       </div>
+
+                    
+                         
+                    </section>
+
+<br><br><br><br><br>
+
+        
+
+<!--footerrrrrrr-->
+<section class="footer">
+    <br><br> <br>
+    <div class="footer-container container">
+        <h2>GlutenFree</h2>
+        <div class="footer-box">
+            <h3>Lien</h3>
+
+            <a href="/maladicoeliaque.html">Maladie coeliaque </a>
+            <a href="/gluten.html">c'est quoi le gluten </a>
+            <a href="/recette.html"> Recette</a>
+
+        </div>
+
+        <div class="footer-box">
+            <h3>location</h3>
+            <a href="#"> ALger </a>
+            <a href="#">Annaba</a>
+            <a href="#">Oran</a>
+
+
+        </div>
+
+
+        <div class="footer-box">
+            <h3>Contact</h3>
+            <a href="#">+213 (0)987 665 6543</a>
+            <a href="#">glutenfree@gmail.com</a>
+            <div class="social">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+</section>
+
+
+
+</body>
+</html>
